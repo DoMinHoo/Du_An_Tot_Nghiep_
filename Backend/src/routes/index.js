245fbcb35bb86a329ffb0e03185cd262b0ac9866
luongpham0910 <luongpvph53397@gmail.com>
2025-05-29 17:50:29 +0700

    api user

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userRouter = require('./auth.routes');
 const categoryRouter = require('./category.routes');
+const userRoutes = require("./user.routes");
 
 router.use('/auth', userRouter);
 router.use('/categories', categoryRouter);
-
+router.use("/users", userRoutes);
 
 
 module.exports = router;
