5a73842cd088ef77108e1e9aef0bfa33bee61256
PhanHuyHoat <hoatphph53239@gmail.com>
2025-06-18 21:53:18 +0700

    sua app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,20 +9,43 @@ import Register from './Pages/Register/register';
 import Login from './Pages/Login/login';
 import SetUser from './SetUser';
 
-import CartPage from './Pages/cart';
+import CartPage from './Pages/Cart/CartPage';
 import BannerSlider from './Pages/Banner';
-
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<> <BannerSlider /> <Home /> </>} />
+        <Route
+          index
+          element={
+            <>
+              {' '}
+              <BannerSlider /> <Home />{' '}
+            </>
+          }
+        />
         <Route path="about" element={<About />} />
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/categories/:slug" element={<> <BannerSlider /> <CategoryPage /> </>} />
-        <Route path="/categories" element={<> <BannerSlider /> <CategoryPage /> </>} />
+        <Route
+          path="/categories/:slug"
+          element={
+            <>
+              {' '}
+              <BannerSlider /> <CategoryPage />{' '}
+            </>
+          }
+        />
+        <Route
+          path="/categories"
+          element={
+            <>
+              {' '}
+              <BannerSlider /> <CategoryPage />{' '}
+            </>
+          }
+        />
 
         {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
       </Route>
